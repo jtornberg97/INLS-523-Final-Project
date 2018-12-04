c0f14cc1711ba1187cd370d391b5e8d2ac881b0b
Jonathan Tornberg <jmtornberg@gmail.com>
2018-12-03 22:26:05 -0500

    Value Insertions

--- Table Creation Queries/instructor_types.sql
+++ Table Creation Queries/instructor_types.sql
@@ -1,9 +1,5 @@
 CREATE TABLE instructor_types
 (
-	id	INT NOT NULL	AUTO_INCREMENT	PRIMARY KEY,
-    adjunct CHAR(7)	NOT NULL,
-    graduate CHAR(8)	NOT NULL, 
-    visiting CHAR(8)	NOT NULL,
-    associate	CHAR(9)	NOT NULL,
-    full	CHAR(4) NOT NULL
+	instructor_id	INT	REFERENCES instructors(id),
+    type	enum('adjunct', 'graduate', 'visitng', 'associate', 'full') NOT NULL
 );
